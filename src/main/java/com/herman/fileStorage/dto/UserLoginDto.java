16b92f7e185bb9d34d343e21c3635d2ba80e030b
Herman2001 <102909811+Herman2001@users.noreply.github.com>
2026-02-05 17:05:42 +0100

    FIX removed @Jsonignore from register and login dto... xD

--- src/main/java/com/herman/fileStorage/dto/UserLoginDto.java
+++ src/main/java/com/herman/fileStorage/dto/UserLoginDto.java
@@ -1,8 +1,6 @@
 package com.herman.fileStorage.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public record UserLoginDto(
         String username,
-        @JsonIgnore String password
+        String password
 ) {}
